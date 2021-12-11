7b084a5773978984a887b8e853f602175cde8478
Mahendrakar <Mahendrakar@LAPTOP-DPHUNK0U>
2021-12-11 10:44:57 +0530

    Update project

---
+++ Mavan_GitHub_Repository/src/main/java/Test/Mavan_GitHub_Repository/App.java
@@ -0,0 +1,13 @@
+package Test.Mavan_GitHub_Repository;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
